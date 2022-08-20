3671fd9eb2c78c5510e40f0d90a99bd67761d26e
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-20 07:29:40 +0300

    This is the solution to task 2.2.1.

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -14,14 +14,10 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru",
-              new Car("car1", 1)));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru",
-              new Car("car2", 2)));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru",
-              new Car("car3", 3)));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru",
-              new Car("car4", 4)));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("car1", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("car2", 2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("car3", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("car4", 4)));
 
       List<User> users = userService.listUsers();
 
